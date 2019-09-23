338fb28bd5a173a4e3a22fbce658e6c389ae6420
Lergin <malte@laukoetter.eu>
2019-09-23 11:52:48 +0200

    Add leaderboard view

---
+++ src/components/PlayerCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-row>
+      <v-col class="shrink ml-2">
+        <v-avatar size="96">
+          <img
+            :src="`https://crafatar.com/avatars/${uuid}`"
+            :alt="`Minecraft Skin Head of ${name}`"/>
+        </v-avatar>
+        <div class="mt-2 grey--text text--darken-2 font-weight-light">{{name}}</div>
+      </v-col>
+      <v-col class="d-flex flex-column justify-end align-end mr-2">
+        <slot></slot>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import '../components/uuid-format';
+
+export default Vue.extend({
+  props: {
+    uuid: String,
+    name: String,
+    rank: String,
+    hideInfoIcon: Boolean
+  },
+
+  data: () => ({
+  }),
+
+  computed: {
+    avatarUrl () {
+      if (this.uuid) {
+        return `https://cravatar.eu/avatar/${this.uuid}`
+      }
+
+      return `https://cravatar.eu/avatar/steve`
+    },
+  },
+});
+</script>
---
+++ src/views/Leaderboard.vue
@@ -0,0 +1,85 @@
+<style scoped>
+  .place-1 {
+    background-color: #d7c05b;
+  }
+
+  .place-2 {
+    background-color: #e6e8fa;
+  }
+
+  .place-3 {
+    background-color: #b4a17e;
+  }
+
+  .full-height {
+    height: 100%;
+  }
+</style>
+
+<template>
+  <div class="full-height">
+    <loading-circular :loading="data.length === 0"></loading-circular>
+
+    <v-row v-if="data.length > 0">
+      <v-col cols="12" sm="4" v-for="i in 3" :key="i">
+        <player-card :class="{[`place-${i}`]: true}" :uuid="data[i - 1].uuid" :name="data[i - 1].name"> 
+          <div class="headline">{{data[i - 1].value | toLocaleString}}</div>
+          <div class="subtitle">{{propertyTitle}}</div>
+        </player-card>
+      </v-col>
+      <v-col cols="12">
+        <leaderboard-card
+          :data="data"
+          :property-title="propertyTitle"
+          :size="97"
+          :offset="3"
+        ></leaderboard-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import LeaderboardCard from "../components/LeaderboardCard.vue";
+import LoadingCircular from "../components/LoadingCircular.vue";
+import PlayerCard from "../components/PlayerCard.vue";
+
+type LeaderboardEntry = {
+  _place: number;
+  name: string;
+  value: string;
+  uuid: string;
+}
+
+@Component({
+  components: {
+    LeaderboardCard,
+    PlayerCard,
+    LoadingCircular
+  }
+})
+export default class Leaderboard extends Vue {
+  @Prop({type: String})
+  readonly id!: string
+
+  @Prop({type: String, default: ''})
+  readonly propertyTitle!: string
+
+  @Prop({type: String, default: ''})
+  readonly title!: string
+
+  private data: LeaderboardEntry[] = []
+
+  async fetchLeaderboard(): Promise<void> {
+    if (this.id == null) return
+
+    this.data = await fetch(`https://api.lergin.de/hive/leaderboard/${this.id}?limit=100`).then(res => res.json());
+  }
+
+  @Watch('id', {immediate: true})
+  onIdChange() {
+    this.fetchLeaderboard()
+  }
+}
+</script>
