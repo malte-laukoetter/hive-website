09bdff86006ab8c7d107ade9f25da1f8dce74e44
Malte Laukötter <malte.laukoetter@gmail.com>
2019-10-11 11:08:27 +0200

    Add service worker runtime cacheing

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,53 @@ module.exports = {
         data: `@import "~@/assets/main.scss"`
       }
     }
+  },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+      cleanupOutdatedCaches: true,
+      runtimeCaching: [{
+        urlPattern: /api/,
+        handler: 'NetworkFirst',
+        options: {
+          networkTimeoutSeconds: 10,
+          cacheName: 'hive-api-cache',
+          cacheableResponse: {
+            statuses: [0, 200, 404]
+          },
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60*60*24,
+          }
+        }
+      }, {
+        urlPattern: new RegExp('^https://cravatar\.com/'),
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200]
+          },
+          cacheName: 'cravatar-cache',
+          expiration: {
+            maxEntries: 1000,
+            maxAgeSeconds: 60*60*24*30,
+          }
+        }
+      }, {
+        urlPattern: new RegExp('^https://api\.lergin\.de/'),
+        handler: 'NetworkFirst',
+        options: {
+          networkTimeoutSeconds: 10,
+          cacheName: 'lergin-api-cache',
+          cacheableResponse: {
+            statuses: [0, 200]
+          },
+          expiration: {
+            maxEntries: 25,
+            maxAgeSeconds: 60*60*24,
+          }
+        }
+      }]
+    }
   }
 };
