14ed2bc40cb55a1ecb4046f73dac2903071bfe48
Malte Laukötter <malte@laukoetter.eu>
2021-01-30 21:59:13 +0100

    Add closing message

--- src/components/App.vue
+++ src/components/App.vue
@@ -38,6 +38,9 @@
       <v-container>
         <div class="headline font-weight-light">{{ name || $route.name }}</div>
         <hive-breadcrumb></hive-breadcrumb>
+        <v-alert type="warning">
+          Hive: Java Edition will be shutting down at April 25th 2020. This site will stop updating any data some days earlier and will be shut down as well some time after the server has been closed. You can find more information about this in the forums: <a href="https://forum.hivemc.com/threads/hive-java-edition.339307/">https://forum.hivemc.com/threads/hive-java-edition.339307/</a> 
+        </v-alert>
         <slot></slot>
       </v-container>
     </v-content>
