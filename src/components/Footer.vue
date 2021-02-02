db932fe6022a5f198119f5da3d36795f63dde2b0
Malte Laukötter <malte@laukoetter.eu>
2021-02-02 13:22:30 +0100

    Remove privacy policy change button
    Data collection is disabled always

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,7 @@
     <v-col class="text-center" cols="12">
       <router-link to="/faq">FAQ</router-link> —
       <a href="https://m-l.dev/imprint">Imprint</a> —
-      <a href="https://m-l.dev/privacy">Privacy</a> —
-      <a class="change-consent" onclick="window.__cmp('displayConsentUi');"
-        >Change Privacy Settings</a
-      >
+      <a href="https://m-l.dev/privacy">Privacy Policy</a>
     </v-col>
   </v-footer>
 </template>
