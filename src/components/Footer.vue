b119d0e1e71e79b92da5c64dc956858cdb5a8e44
Lergin <malte@laukoetter.eu>
2021-04-02 22:37:47 +0200

    Format code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,9 +4,9 @@
       <router-link to="/faq">FAQ</router-link> —
       <a href="https://m-l.dev/imprint">Imprint</a> —
       <a href="https://m-l.dev/privacy">Privacy Policy</a> —
-      <span class="text-overline">{{commitRef}}</span> —
-      <span class="text-overline">{{buildId}}</span> —
-      <span class="text-overline">{{buildDate}}</span>
+      <span class="text-overline">{{ commitRef }}</span> —
+      <span class="text-overline">{{ buildId }}</span> —
+      <span class="text-overline">{{ buildDate }}</span>
     </v-col>
   </v-footer>
 </template>
@@ -16,8 +16,8 @@ import { Vue, Component } from "vue-property-decorator";
 
 @Component
 export default class Footer extends Vue {
-  commitRef: string = process.env.VUE_APP_COMMIT_REF.substring(0,6)
-  buildId: string = process.env.VUE_APP_BUILD_ID.substring(0,6)
-  buildDate: string = process.env.VUE_APP_BUILD_DATE
+  commitRef: string = process.env.VUE_APP_COMMIT_REF.substring(0, 6);
+  buildId: string = process.env.VUE_APP_BUILD_ID.substring(0, 6);
+  buildDate: string = process.env.VUE_APP_BUILD_DATE;
 }
 </script>
