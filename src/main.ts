f1702eaa9d9d8cad84f694151852bc187eba3d01
Lergin <malte@laukoetter.eu>
2021-04-29 20:03:47 +0200

    Remove remaining firebase dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 import vuetify from "./plugins/vuetify";
-import * as firebase from "firebase/app";
 import VueMeta from "vue-meta";
 import { Methods } from "hive-api/dist/hive.min.js";
 
@@ -13,18 +12,6 @@ if (location.hostname !== "localhost") {
 
 import "./assets/style.css";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBPl570AoqaHuCgX342wICImCJbz4c3vFs",
-  authDomain: "hivewebsite-1da4d.firebaseapp.com",
-  databaseURL: "https://hivewebsite-1da4d.firebaseio.com",
-  projectId: "hivewebsite-1da4d",
-  storageBucket: "hivewebsite-1da4d.appspot.com",
-  messagingSenderId: "836246928386",
-  appId: "1:836246928386:web:ec67fdff6c8f63c6aebb1b",
-  measurementId: "G-YNMZTKC3VM"
-};
-
-firebase.initializeApp(firebaseConfig);
 Vue.config.productionTip = false;
 Vue.use(VueMeta);
 
@@ -33,14 +20,6 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount("#app");
-
-router.afterEach(async to => {
-  await Vue.nextTick();
-  (window as any).gtag("config", "UA-60277501-8", {
-    anonymize_ip: true,
-    page_path: to.fullPath
-  });
-});
 
 Vue.filter("toLocaleString", function(value: any) {
   if (value == null) return "";
